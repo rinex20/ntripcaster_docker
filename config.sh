718ca37c1c055d21f786a6c87097d3a18c9bbc3f
rinex20 <rinex20@me.com>
2020-03-31 13:33:29 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@
 echo "Downloading configuration example files..."
 cd ~
 wget https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf.zip
+mkdir -p /etc/ntripcaster
 echo "Unzip configuration files..."
 unzip conf.zip -d /etc/ntripcaster
 clear && ls /etc/ntripcaster/conf
