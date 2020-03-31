d535b7b71fbdb791f845616ca3d6e940c524c17f
rinex20 <rinex20@me.com>
2020-03-31 14:17:29 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 
 echo "Downloading configuration example files..."
 mkdir -p /etc/ntripcaster/conf
-wget -P /etc/ntripcaster/conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/users.aut
-wget -P /etc/ntripcaster/conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/groups.aut
-wget -P /etc/ntripcaster/conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/clientmounts.aut
-wget -P /etc/ntripcaster/conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/sourcemounts.aut
-wget -P /etc/ntripcaster/conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/sourcetable.dat
-wget -P /etc/ntripcaster/conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/ntripcaster.conf
+wget -O /etc/ntripcaster/conf/users.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/users.aut
+wget -O /etc/ntripcaster/conf/groups.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/groups.aut
+wget -O /etc/ntripcaster/conf/clientmounts.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/clientmounts.aut
+wget -O /etc/ntripcaster/conf/sourcemounts.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/sourcemounts.aut
+wget -O /etc/ntripcaster/conf/sourcetable.dat https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/sourcetable.dat
+wget -O /etc/ntripcaster/conf/ntripcaster.conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/ntripcaster.conf
 
 ls -l /etc/ntripcaster/conf
